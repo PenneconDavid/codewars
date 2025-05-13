2a0a6a760d5c72eab4a77f85cf6305f962e47413
David Sebold <d.seibold@icloud.com>
2025-05-13 01:40:02 -0700

    daily challenge done

---
+++ Solutions/ReplaceWithAlphabetPosition.js
@@ -0,0 +1,8 @@
+function alphabetPosition(text) {
+  return text
+    .toLowerCase()
+    .split("")
+    .filter((char) => /[a-z]/.test(char))
+    .map((char) => char.charCodeAt(0) - 96)
+    .join(" ");
+}
